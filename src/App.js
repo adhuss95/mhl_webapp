15c324ce72b672e2159ce2a39431d3efac8a3c63
Adhithya Ramesh <adhithya@mythopoeia.ie>
2019-04-18 17:54:56 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import ReactTable from "react-table";
+import Hamoni from "hamoni-sync";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <h1>MY Application</h1> 
+      </div>
+    );
+  }
+}
+
+export default App;
